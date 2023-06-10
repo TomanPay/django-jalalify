078511b6961688da6ac67c0a60ffa8b644b414d8
Mohammad Javad Nikbakht <javadnikbakht@mail.com>
2023-06-11 00:38:21 +0330

    Bump version: 1.1.0 → 1.1.1

--- django_jalalify/__init__.py
+++ django_jalalify/__init__.py
@@ -3,6 +3,6 @@ from khayyam import JalaliDate, JalaliDatetime
 __author__ = "Mohammad Javad Nikbakht"
 __email__ = "javadnikbakht@mail.com"
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 __all__ = ["JalaliDate", "JalaliDatetime"]
